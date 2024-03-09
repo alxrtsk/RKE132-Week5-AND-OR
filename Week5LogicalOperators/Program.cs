679209e18ddf7395ce7d720a28aa0e44c184c8d6
alexr <reitsak.alex@gmail.com>
2024-03-09 16:06:23 +0200

    f

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,10 @@
+﻿// AND - &&
+// true && true -> true
+// true && false -> false
+// false && true -> false
+
+// OR - || (pipes)
+// true || true -> true
+// true || false -> true
+// false || true -> true
+// false || false -> false
